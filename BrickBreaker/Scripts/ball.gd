55077d8923f54b35e2cdfbccf1f318d20c99e810
joyfaithh <122395454+joyfaithh@users.noreply.github.com>
2023-11-17 13:57:41 -0800

    Better ball collision - DONE

--- BrickBreaker/Scripts/ball.gd
+++ BrickBreaker/Scripts/ball.gd
@@ -6,16 +6,20 @@ signal life_lost
 
 const VELOCITY_LIMIT = 40
 
-@export var ball_speed = 20
+@export var ball_speed = 15
 @export var lifes = 3
 @export var death_zone: DeathZone
 @export var ui: UI
 
 var speed_up_factor = 1.05
+var start_position: Vector2
+var last_collider_id
 
 @onready var collision_shape_2d = $CollisionShape2D
 
-var start_position: Vector2
+
+
+
 func _ready():
 	ui.set_lifes(lifes)
 	start_position = position
@@ -30,13 +34,17 @@ func _physics_process(delta):
 	if collider is Brick:
 		collider.decrease_level()
 		
-	velocity = velocity.bounce(collision.get_normal())
+	if (collider is Brick or collider is Paddle): 
+		ball_collision(collider)
+	else:
+		velocity = velocity.bounce(collision.get_normal())
+	
 	
 func start_ball():
 	position = start_position
 	randomize()
 	
-	velocity = Vector2(randf_range(-1, 1), randf_range(-.1, -1_)).normalized() * ball_speed
+	velocity = Vector2(randf_range(-1, 1), randf_range(-0.1, -1)).normalized() * ball_speed
 
 func on_life_lost():
 	lifes -= 1
@@ -50,3 +58,27 @@ func on_life_lost():
 func reset_ball():
 	position = start_position
 	velocity = Vector2.ZERO
+
+func ball_collision(collider):
+	var ball_width = collision_shape_2d.shape.get_rect().size.x
+	var ball_center_x = position.x
+	var collider_width = collider.get_width()
+	var collider_center_x = collider.position.x
+	
+	var velocity_xy = velocity.length()
+	
+	var collision_x = (ball_center_x - collider_center_x) / (collider_width / 2)
+	
+	var new_velocity = Vector2.ZERO
+	
+	new_velocity.x = velocity_xy * collision_x
+	
+	if collider.get_rid() == last_collider_id && collider is Brick:
+		new_velocity.x = new_velocity.rotated(deg_to_rad(randf_range(-45, 45))).x * 10
+	else:
+		last_collider_id = collider.get_rid()  # Fix: Use '=' instead of '==' to assign the value
+
+	new_velocity.y = sqrt(absf(velocity_xy * velocity_xy - new_velocity.x * new_velocity.x)) * (-1 if velocity.y > 0 else 1)
+	var speed_multiplier = 1.0  # Default multiplier
+
+	velocity = (new_velocity * speed_multiplier).limit_length(VELOCITY_LIMIT)
