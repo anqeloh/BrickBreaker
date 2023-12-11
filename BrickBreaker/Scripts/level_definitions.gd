83e544b48ff9e8992660655d163af762bd9317b5
anqeloh <122122517+anqeloh@users.noreply.github.com>
2023-12-10 23:19:51 -0800

    COMPLETED BRICK BREAKER

--- BrickBreaker/Scripts/level_definitions.gd
+++ BrickBreaker/Scripts/level_definitions.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var current_level = 2
+var current_level = 3
 
 var level_1 = [
 	[0,1,0]
@@ -14,7 +14,14 @@ var level_2 = [
 	[1,1,1,1,1,1,1,1]
 ]
 
-var levels = [level_1, level_2]
+var level_3 = [
+	[5,5,5,0,0,5,5,5],
+	[5,5,4,0,0,5,5,5],
+	[3,3,5,0,0,3,3,3],
+	[2,2,2,4,4,4,5,5],
+	[0,1,1,1,1,1,1,0]
+	]
+var levels = [level_1, level_2, level_3]
 
 func get_current_level():
 	print(levels[current_level - 1])
